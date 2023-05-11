7a253d85028118573abf498b2f5fdc4a1daecb7a
Dima <dimagolt@gmail.com>
2023-05-11 17:36:35 +0200

    add basic movement

--- Assets/WormController.cs
+++ Assets/WormController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class WormController : MonoBehaviour
 {
     Rigidbody2D rigidbody;
+    [SerializeField]
+    bool isBouncing;
+    bool onGround;
+    bool isJumping;
 
     private void Start()
     {
@@ -14,5 +18,44 @@ public class WormController : MonoBehaviour
     {
         var movement = Input.GetAxis("Horizontal");
         rigidbody.velocity += new Vector2(movement, 0);
+        if (Input.GetKey(KeyCode.UpArrow) && onGround && !isJumping)
+        {
+            isJumping = true;
+            Invoke(nameof(Jump), 1f);
+        }
+    
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ( collision.gameObject.tag == "Wall")
+        {
+            float bounce = 600f; //amount of force to apply
+            rigidbody.AddForce(collision.contacts[0].normal * bounce);
+            isBouncing = true;
+            Invoke("StopBounce", 1f);
+        }
+        if (collision.gameObject.tag == "Floor")
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            onGround = false;
+        }
+    }
+
+    void StopBounce()
+    {
+        isBouncing = false;
+    }
+
+    void Jump() {
+        rigidbody.AddForce(new Vector2(0, 500));
+        isJumping = false;
     }
 }
