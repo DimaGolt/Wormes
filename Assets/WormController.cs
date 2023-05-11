1f5c0919850f0f954779682e2d1ab57819d14a2a
Dima <dimagolt@gmail.com>
2023-05-11 16:42:28 +0200

    initial commit

---
+++ Assets/WormController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        rigidbody.velocity += new Vector2(movement, 0);
+    }
+}
