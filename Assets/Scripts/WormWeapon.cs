3bbd965b0caa5e29b24498824c4d653e81807c48
Dima_Golt <dimagolt@gmail.com>
2023-06-18 17:58:16 +0200

    add weapon change (bazooka only)

---
+++ Assets/Scripts/WormWeapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormWeapon : MonoBehaviour
+{
+    private int _selectedWeapon = 0;
+    [SerializeField]
+    public RuntimeAnimatorController empty;
+    [SerializeField]
+    public RuntimeAnimatorController bazooka;
+
+    private Animator _animator;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        switch (_selectedWeapon)
+        {
+            case 1:
+                _animator.runtimeAnimatorController = bazooka;
+                break;
+            default:
+                _animator.runtimeAnimatorController = empty;
+                break;
+        }
+    }
+
+    public void ChangeWeapon(int weapon)
+    {
+        _selectedWeapon = weapon;
+    }
+}
