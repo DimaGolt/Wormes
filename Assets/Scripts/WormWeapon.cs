dcd88a3e72617e0895652f9476dd42b14b5c1616
Dima_Golt <dimagolt@gmail.com>
2023-06-18 18:34:41 +0200

    add other weapon animations

--- Assets/Scripts/WormWeapon.cs
+++ Assets/Scripts/WormWeapon.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class WormWeapon : MonoBehaviour
 {
-    private int _selectedWeapon = 0;
+    private int _selectedWeapon = 1;
     [SerializeField]
     public RuntimeAnimatorController empty;
     [SerializeField]
     public RuntimeAnimatorController bazooka;
+    [SerializeField]
+    public RuntimeAnimatorController shotgun;
+    [SerializeField]
+    public RuntimeAnimatorController uzi;
+    [SerializeField]
+    public RuntimeAnimatorController dragon;
 
     private Animator _animator;
     void Start()
@@ -22,7 +28,18 @@ public class WormWeapon : MonoBehaviour
         switch (_selectedWeapon)
         {
             case 1:
+            case 2:
                 _animator.runtimeAnimatorController = bazooka;
+                break;
+            case 5:
+                _animator.runtimeAnimatorController = shotgun;
+                break;
+            case 6:
+                _animator.runtimeAnimatorController = uzi;
+                break;
+            case 7:
+            case 8:
+                _animator.runtimeAnimatorController = dragon;
                 break;
             default:
                 _animator.runtimeAnimatorController = empty;
